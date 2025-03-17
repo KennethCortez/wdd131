a5d26009615e4a1191742a8fe76a6520e03e8c51
KennethCortez <kennethleocortez@gmail.com>
2025-03-16 18:18:33 -0600

    Temples web page

---
+++ scripts/temples.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const currentYear = new Date().getFullYear();
+    const footerText = document.querySelector("footer p:nth-of-type(1)");
+    if (footerText) {
+        footerText.innerHTML = `©️${currentYear} Kenneth Cortez, Sonsonate, El Salvador`;
+    }
+
+    const lastModifiedDate = new Date(document.lastModified);
+    const formattedDate = lastModifiedDate.toLocaleString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+    });
+
+    const lastModifiedElement = document.getElementById("lastModified");
+    if (lastModifiedElement) {
+        lastModifiedElement.innerHTML = `Last Modified: ${formattedDate}`;
+    }
+
+    // hamburguer menu
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    if (hamburger && navMenu) {
+        hamburger.addEventListener("click", function () {
+            // Cambiar la visibilidad del menú cuando se hace clic en el botón hamburguesa
+            navMenu.classList.toggle("open");
+            const isOpen = navMenu.classList.contains("open");
+            hamburger.setAttribute("aria-expanded", isOpen);
+        });
+    }
+});
